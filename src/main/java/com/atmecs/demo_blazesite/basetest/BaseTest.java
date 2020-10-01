e8ac0160ad0de6c32ea3d4330588aceaef8cbf84
ATMECS\Alfin.Anthonyraj <alfin.anthonyraj@atmecs.com>
2020-10-01 15:03:00 +0530

    Updated

--- src/main/java/com/atmecs/demo_blazesite/basetest/BaseTest.java
+++ src/main/java/com/atmecs/demo_blazesite/basetest/BaseTest.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import com.atmecs.demo_blazesite.constants.Constants;
@@ -28,6 +29,8 @@ public class BaseTest
 		System.setProperty(Constants.USER_DIR, Constants.CHROME_PATH);
 		driver = new ChromeDriver();
 		driver.get(baseUrl);
+		
+		Assert.assertEquals(driver.getCurrentUrl(), baseUrl);
 		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(150, TimeUnit.SECONDS);
